8497bd4eb2b36aca99b98699602b09eb48f7829f
Irfon Baig <md.irfonbaig@gmail.com>
2020-03-06 21:10:56 +0530

    Adding All Spring Boot Restful Webservices Apps

---
+++ src/test/java/com/example/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
